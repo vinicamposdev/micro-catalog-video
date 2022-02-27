69d1b9a29af3f3ec1572cf19bd433fe444bb9eaf
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2022-02-27 17:56:00 -0300

    feat: bootstrap Spring Initializr

---
+++ src/main/java/com/codeflix/VideoCatalog/VideoCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.codeflix.VideoCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeflix/VideoCatalog/VideoCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeflix.VideoCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
