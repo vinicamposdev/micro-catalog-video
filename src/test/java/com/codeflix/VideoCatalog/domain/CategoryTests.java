623bdc223780ab2a7d927c02b1c9538980f69717
Vinicius Campos <vinicius.campos@tech4h.com.br>
2022-04-11 16:40:02 -0300

    test: ensure category throws a domain exception when name is null or empty

--- src/test/java/com/codeflix/VideoCatalog/domain/CategoryTests.java
+++ src/test/java/com/codeflix/VideoCatalog/domain/CategoryTests.java
@@ -3,6 +3,7 @@ package com.codeflix.VideoCatalog.domain;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
@@ -16,10 +17,21 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 class CategoryTests {
+	@Test
+	@DisplayName("it should ensure category throws domain exception when name is null")
+	void throwDomainExeptionWhenNameIsNull() {
+		assertThrows(DomainException.class, () -> new Category(null, "any_description"));
+	}
+
+	@Test
+	@DisplayName("it should ensure category throws domain exception when name is empty")
+	void throwDomainExeptionWhenNameIsBlank() {
+		assertThrows(DomainException.class, () -> new Category("", "any_description"));
+	}
 
 	@Test
 	@DisplayName("it should ensure that a category is created with name, description and isActive set to true")
-	void createCategoryWithNameDescription() throws Exception {
+	void createCategoryWithNameDescription() {
 		final Category entity = new Category("any_name", "any_description");
 
 		assertNotNull(entity);
@@ -30,8 +42,8 @@ class CategoryTests {
 
 	@Test
 	@DisplayName("it should ensure that method deactivate returns isActive false")
-	void createCategoryAndIsDeactivateTrue() throws Exception {
-		final Category entity = new Category("any_name", "any_description");
+	void createCategoryAndIsDeactivateTrue() {
+		final Category entity = new Category("any_name", "any_description"); 
 
 		entity.deactivate();
 
@@ -41,7 +53,7 @@ class CategoryTests {
 
 	@Test
 	@DisplayName("it should ensure that method update returns updated values")
-	void createCategoryAndUpdate() throws Exception {
+	void createCategoryAndUpdate() {
 		final Category entity = new Category("any_name", "any_description");
 
 		entity.update("any_name_updated", "any_description_updated", false);
