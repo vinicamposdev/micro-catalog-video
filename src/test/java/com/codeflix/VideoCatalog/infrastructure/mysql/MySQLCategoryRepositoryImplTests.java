cd719be60777ab6e6255eb75fe681a1d2d608047
Vinicius Campos <vinicius.campos@tech4h.com.br>
2022-04-15 12:34:52 -0300

    test: :white_check_mark: ensure MySQLCategoryRepositoryImpl returns finded categories

--- src/test/java/com/codeflix/VideoCatalog/infrastructure/mysql/MySQLCategoryRepositoryImplTests.java
+++ src/test/java/com/codeflix/VideoCatalog/infrastructure/mysql/MySQLCategoryRepositoryImplTests.java
@@ -5,6 +5,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.codeflix.VideoCatalog.domain.entity.Category;
 import com.codeflix.VideoCatalog.infrastructure.data.SpringDataCategoryRepository;
 import com.codeflix.VideoCatalog.infrastructure.persistence.CategoryPersistence;
@@ -40,6 +43,26 @@ public class MySQLCategoryRepositoryImplTests {
         assertThat(actual.getName()).isEqualTo(expected.getName());
         assertThat(actual).hasFieldOrPropertyWithValue("description", expected.getDescription());
 
+    }
+
+
+    @Test
+    @DisplayName("it should ensure MySQLCategoryRepositoryImpl returns finded categories")
+    public void executeReturnsFindAllCategories() {
+        Category entity1 = new Category("any_name_1", "any_description_1");
+        Category entiry2 = new Category("any_name_2", "any_description_2");
+        List <CategoryPersistence> expected = new ArrayList<CategoryPersistence>();
+        expected.add(CategoryPersistence.from(entity1));
+        expected.add(CategoryPersistence.from(entiry2));
+
+        doReturn(expected)
+            .when(springDataCategoryRepository)
+            .findAll();
+        List<Category> actual = repository.findAll();
+
+        assertThat(actual).isNotNull();
+        assertThat(actual).isNotEmpty();
+        assertThat(actual).hasSize(2);
     }
 }
    
